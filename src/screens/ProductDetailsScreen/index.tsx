da5bf3f763cb4327ff9986c24f12690c5412abf3
CaptainBlack21 <omeryyagmur>
2024-11-11 14:54:52 +0300

    MessageBox added

--- src/screens/ProductDetailsScreen/index.tsx
+++ src/screens/ProductDetailsScreen/index.tsx
@@ -4,20 +4,25 @@ import ImageCarousel from "../../components/ImageCarousel"
 import DetailTextBox from "../../components/DetailTextBox"
 import SellerProfile from "../../components/SellerProfile"
 import MappingComponent from "../../components/MappingComponent"
-
+import SocialMedias from "../../components/SocialMedias"
+import MessageBox from "../../components/MessageBox"
 
 import { Product } from '../../models'
 function index(props) {
     const [product, setProduct] = useState<Product>(props.route.params.product)
     return (
-        <ScrollView style={{ flex: 1, backgroundColor: "white" }}>
-            <ImageCarousel images={product.images} />
-            <View style={{paddingHorizontal:20,paddingVertical:14}}>
-                <DetailTextBox price={product.price} name={product.name} description={product.description} />
-                <SellerProfile averageRating={product.rating}/>
-                <MappingComponent/>
-            </View>
-        </ScrollView>
+        <View style={{flex:1}}>
+            <ScrollView style={{ flex: 1, backgroundColor: "white",marginBottom:"28%" }}>
+                <ImageCarousel images={product.images} />
+                <View style={{ paddingHorizontal: 20, paddingVertical: 14 }}>
+                    <DetailTextBox price={product.price} name={product.name} description={product.description} />
+                    <SellerProfile averageRating={product.rating} />
+                    <MappingComponent />
+                    <SocialMedias />
+                </View>
+            </ScrollView>
+            <MessageBox/>
+        </View>
     )
 }
 
